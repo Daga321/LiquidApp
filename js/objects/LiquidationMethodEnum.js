8ce405abfa9ff43ef18b3bdd070440dfbff0b86e
Daniel Alejandro Gomez Acero <73965157+Daga321@users.noreply.github.com>
2025-07-02 06:22:03 +0000

    remove the page from main branch

--- js/objects/LiquidationMethodEnum.js
+++
@@ -1,7 +0,0 @@
-class LiquidationMethodEnum{
-    
-    static CONSUMPTION = {Key: "CONSUMPTION", Method: "Por consumo", InputPlaceHolder: "Unidades consumidas"}
-    static PERCENTAGE = {Key: "PERCENTAGE", Method: "Por porcentaje", InputPlaceHolder: "Porcentaje asignado"}
-    static PEOPLE = {Key: "PEOPLE", Method: "Por número de personas", InputPlaceHolder: "Numero de personas"}
-
-}
